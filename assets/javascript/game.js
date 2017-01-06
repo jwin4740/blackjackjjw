ad5874d8a467a50c2339dca8fb37a57c3aba42e0
James <jwin4740@gmail.com>
2017-01-05 22:51:46 -0500

    jack

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+
+for (var i = 1; i < 53; i++)
+{
+	$("#card").append("<img class='snap' alt='cardimage' src='assets/images/card" + i + ".png'>");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
